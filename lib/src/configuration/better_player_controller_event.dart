3ce4b14dea36c211a6ba582f58a5b80d8fa50088
trần xuân thịnh <thinhtx@itsol.vn>
2024-07-04 14:40:33 +0700

    shhhs

---
+++ lib/src/configuration/better_player_controller_event.dart
@@ -0,0 +1,20 @@
+///Internal events of BetterPlayerController, used in widgets to update state.
+enum BetterPlayerControllerEvent {
+  ///Fullscreen mode has started.
+  openFullscreen,
+
+  ///Fullscreen mode has ended.
+  hideFullscreen,
+  
+  showscreen,
+  
+  
+  ///Subtitles changed.
+  changeSubtitles,
+
+  ///New data source has been set.
+  setupDataSource,
+
+  //Video has started.
+  play
+}
